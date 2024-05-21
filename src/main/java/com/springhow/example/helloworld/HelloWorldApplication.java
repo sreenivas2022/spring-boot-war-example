d2dd34ca7c1b4121ee210116d522ae8dd452b2ec
Leela Srinivas <97235203+sreenivas2022@users.noreply.github.com>
2024-05-21 07:51:28 +0530

    Update HelloWorldApplication.java
    update html

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Sreenivas!";
+        return "Hello Leela!";
     }
 
 }
